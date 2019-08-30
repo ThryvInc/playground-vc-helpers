426a3ad58d8bd70066b2d06a78fba3de9fcf0740
Elliot <elliot.schrock@gmail.com>
2019-08-30 14:38:37 -0400

    allow passing the bundle

--- PlaygroundVCHelpers/Classes/PlaygroundVCHelpers.swift
+++ PlaygroundVCHelpers/Classes/PlaygroundVCHelpers.swift
@@ -7,15 +7,13 @@
 
 import Foundation
 
-public func loadViewFromNib<T>(owner: T, nibName: String = String(describing: T.self)) -> UIView? where T: UIViewController {
-    let bundle = Bundle(for: T.self)
+public func loadViewFromNib<T>(owner: T, nibName: String = String(describing: T.self), bundle: Bundle = Bundle(for: T.self)) -> UIView? where T: UIViewController {
     let nib = UINib(nibName: nibName, bundle: bundle)
     let view = nib.instantiate(withOwner: owner, options: nil).first as? UIView
     return view
 }
 
-public func loadVCFromStoryboard<T>(named: String, isInitialVC: Bool = false, storyboardId: String? = nil) -> T? where T: UIViewController {
-    let bundle = Bundle(for: T.self)
+public func loadVCFromStoryboard<T>(named: String, isInitialVC: Bool = false, storyboardId: String? = nil, bundle: Bundle = Bundle(for: T.self)) -> T? where T: UIViewController {
     let storyboard = UIStoryboard(name: named, bundle: bundle)
     if isInitialVC {
         return storyboard.instantiateInitialViewController() as? T
